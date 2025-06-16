de9c52eff1e85e1287a93277eb7226deddd5b9fb
kterraza-2023022 <kterraza-2023022@kinal.edu.gt>
2025-06-15 21:07:07 -0600

    se sube lo del curso de udemy

---
+++ ECOM/App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import MainSatckNavigator from './navigation/MainNavigator';
+
+export default function App() {
+  return (
+    <MainSatckNavigator/>
+  );
+}
